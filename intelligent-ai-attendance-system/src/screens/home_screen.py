2f678b6c86b2e9f3386afe7ceddcf74e6c8ee548
vishalbooraa <vishalboora00@gmail.com>
2026-04-21 12:58:27 +0530

    overridden streamlit css

--- intelligent-ai-attendance-system/src/screens/home_screen.py
+++ intelligent-ai-attendance-system/src/screens/home_screen.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-from src.components.header_home import header_home
+from src.components.header import header_home
+from src.ui.style_base_layout import style_background_home
+from src.ui.style_base_layout import style_base_layout
+
 
 def home_screen():
     st.header("Home Screen")
 
     header_home()
+    style_base_layout()
+    style_background_home()
     col1,col2=st.columns(2)
 
     with col1:
---
+++ intelligent-ai-attendance-system/src/ui/style_base_layout.py
@@ -0,0 +1,83 @@
+import streamlit as st
+
+def style_background_home():
+    st.markdown("""
+    <style>
+        .stApp{
+              background: #5865F2 !important
+            }
+    </style>
+
+""",unsafe_allow_html=True)
+    
+
+def style_background_dashboard():
+    st.markdown("""
+    <style>
+        .stApp{
+              background: #5865F2 !important
+            }
+    </style>
+
+""",unsafe_allow_html=True)
+    
+def style_base_layout():
+    st.markdown("""
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Outfit:wght@100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    /* Hide Top Bar of streamlit */
+    #MainMenu, footer, header {
+        visibility: hidden;
+    }
+    .block-container{
+        padding-top:1.5rem !important        
+        }
+    h1{
+    font-family:'Climate Crisis', sans-serif !important;
+    font-size:3.5rem !important;
+    line-height:1.1 !important;
+    margin-bottom:0rem !important
+    }
+    h2{
+    font-family:'Climate Crisis', sans-serif !important;
+    font-size:3.5rem !important;
+    line-height:1.1 !important;
+    margin-bottom:0rem !important
+    }
+    h3,h4,p,span{
+        font-family:'Outfit', sans-serif; !important
+    }
+    
+    button{
+        background: #5965F2 !important;
+        border-radius:1.5rem !important;
+        color:white !important;
+        padding: 10px 20px !important;
+        border: none !important;
+        transition: transform 0.25s ease-in-out !important;
+    }
+                
+    button[kind="secondary"]{
+        background: #EB459E !important;
+        border-radius:1.5rem !important;
+        color:white !important;
+        padding: 10px 20px !important;
+        border: none !important;
+        transition: transform 0.25s ease-in-out !important;
+    }
+    
+    button[kind="tertiary"]{
+        background: #black !important;
+        border-radius:1.5rem !important;
+        color:white !important;
+        padding: 10px 20px !important;
+        border: none !important;
+        transition: transform 0.25s ease-in-out !important;
+    }
+
+    button:hover{
+        transform: scale(1.05)
+    }
+    </style>
+    """, unsafe_allow_html=True)
